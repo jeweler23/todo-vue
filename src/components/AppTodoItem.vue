88bcdbbcb725ae63bdf8a83d75abe7d51ccfa2a1
 jeweler23 <lopatin.lopatin2011@gmail.com>
2024-03-04 21:53:11 +0300

    fead: add first TS props

---
+++ src/components/AppTodoItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <li class="todo-item todo-item--done">
+      <div class="todo-item__status">
+        <i class="bi bi-check2"></i>
+      </div>
+      <span class="todo-item__text">Learn the basics of Vue</span>
+      <button class="todo-item__remove-button">
+        <i class="bi bi-trash3"></i>
+      </button>
+    </li>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, type PropType } from 'vue'
+
+export default defineComponent({
+  props: {
+    todo: {
+      type: Object as PropType<{
+        id: number
+        text: String
+        complete: boolean
+      }>,
+      required: true
+    }
+  }
+})
+</script>
